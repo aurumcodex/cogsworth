4a115ab0557c82be0041d6262a605b69343e3108
aurumcodex <aurumcodex@protonmail.com>
2020-10-17 21:27:17 -0400

    make bot work (i think)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("bruh");
+use std::env;
+// use std::io;
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*
+};
+
+mod urls;
+// use crate::urls::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "cringe" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, urls::CRINGE).await {
+                println!("Err sending message: {:?}", why);
+            }
+        }
+
+        if msg.content == "bean donut" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, urls::BEAN_DONUT).await {
+                println!("Err sending message: {:?}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected and ready", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("unable to find token via env vars");
+
+    let mut client = Client::new(&token)
+                        .event_handler(Handler)
+                        .await
+                        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client Error: {:?}", why);
+    }
 }
---
+++ src/urls.rs
@@ -0,0 +1,3 @@
+pub const BEAN_DONUT: &str = "https://raw.githubusercontent.com/aurumcodex/cogsworth/main/img/bean_donut.png";
+pub const CRINGE: &str = "https://raw.githubusercontent.com/aurumcodex/cogsworth/main/img/cringe.png";
+pub const SMUG_NIA: &str = "https://raw.githubusercontent.com/aurumcodex/cogsworth/main/img/smug_nia.jpg";
